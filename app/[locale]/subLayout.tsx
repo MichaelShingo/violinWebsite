42434c83ef1b4fe6c9b7444a68ad708df120ab0a
Michael Shingo Crawford <mcrawford5376@gmail.com>
2024-10-19 18:32:45 +0900

    versioning untracked files

--- app/[locale]/subLayout.tsx
+++ app/[locale]/subLayout.tsx
@@ -1,10 +1,10 @@
 'use client';
 import { FC, ReactNode } from "react";
 import BlobMenu from "../components/navbar/BlobMenu";
-import LanguageToggle from "../components/language/LanguageToggle";
 import { ReactLenis } from 'lenis/react';
 import WindowEvents from "../components/eventHandlers/windowEvents";
 import Modal from "../components/modal/Modal";
+import LanguageToggle from "../components/language/LanguageToggle";
 
 interface SubLayoutProps {
     children: ReactNode;
