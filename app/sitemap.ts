82fe03bcc3b69e508b0e47b3053ad1affa95a237
Michael Shingo Crawford <mcrawford5376@gmail.com>
2024-10-29 22:24:27 +0900

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,9 +1,23 @@
 import { MetadataRoute } from 'next';
-
+import { urls } from './constants/urls';
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+	const calcPriority = (url: string) => {
+		if (url === urls.home || url === urls.violinist) {
+			return 1;
+		} else {
+			return 0.8;
+		}
+	};
+	const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+	const fullUrls = Object.values(urls).map(path => {
+		return {
+			url: `${baseUrl}${path}`,
+			lastModified: new Date(),
+			priority: calcPriority(path)
+		};
+	});
+
 	return [
-		{
-			url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
-		},
+		...fullUrls
 	];
 }
